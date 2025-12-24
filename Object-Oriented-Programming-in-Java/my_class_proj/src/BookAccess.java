57347cde253c17fff27c7b198085d34ab0f73a82
Mehmet Tarhan <hi@memtarhan.com>
2025-12-24 13:22:38 +0300

    <Object Oriented Programming> - Build Your Own Java Constructor

--- Object-Oriented-Programming-in-Java/my_class_proj/src/BookAccess.java
+++ Object-Oriented-Programming-in-Java/my_class_proj/src/BookAccess.java
@@ -1,23 +1,6 @@
 public class BookAccess {
-    private static void comparePrices(Book book1, Book book2) {
-        float book1Price = book1.getPrice();
-        float book2Price = book2.getPrice();
-        String priceCmp = "";
-        if (book1Price < book2Price) {
-            priceCmp = book1.getTitle()+" costs less than " + book2.getTitle();
-        } else if (book1Price == book2Price) {
-            priceCmp = book1.getTitle()+" costs as much as " + book2.getTitle();
-        } else {
-            priceCmp = book1.getTitle()+" costs more than " + book2.getTitle();
-        }
-        System.out.println(priceCmp);
-    }
-
-    public static void main(String s[]) {
-        Book book1 = new Book();
-        book1.setTitle("Atomic Habits");
-        book1.setAuthor("James Clear");
-        book1.setPrice(30.00f);
+    public static void main(String s[]) throws CloneNotSupportedException {
+        Book book1 = new Book("Atomic Habits", "James Clear", 30.00f);
 
         Book book2 = new Book();
         book2.setTitle("Sapiens");
@@ -28,6 +11,10 @@ public class BookAccess {
         System.out.println(book1);
         System.out.println("The second book object is ");
         System.out.println(book2);
-        comparePrices(book1, book2);
+
+        Book book3 = (Book)(book1.clone());
+        System.out.println("The third book cloned object is ");
+        System.out.println(book3);
+
     }
 }
